ff3212282d10634678196f8b6c8ddd2f1e5b9771
Alexander <a-l-e-k-s@yandex.ru>
2021-07-22 11:44:28 +0300

    Fixed some bugs

--- HotWaterWillBack/Helpers/SplitDateHelper.swift
+++ HotWaterWillBack/Helpers/SplitDateHelper.swift
@@ -11,22 +11,22 @@ struct SplitDateHelper {
     let fullString: String
     
     var year: String {
-        String(fullString[...fullString.index(fullString.startIndex, offsetBy: 3)])
+        fullString.count > 14 ? String(fullString[...fullString.index(fullString.startIndex, offsetBy: 3)]) : ""
     }
     
     var month: String {
-        String(fullString[fullString.index(fullString.startIndex, offsetBy: 5)...fullString.index(fullString.startIndex, offsetBy: 6)])
+        fullString.count > 14 ? String(fullString[fullString.index(fullString.startIndex, offsetBy: 5)...fullString.index(fullString.startIndex, offsetBy: 6)]) : ""
     }
     
     var day: String {
-        String(fullString[fullString.index(fullString.startIndex, offsetBy: 8)...fullString.index(fullString.startIndex, offsetBy: 9)])
+        fullString.count > 14 ? String(fullString[fullString.index(fullString.startIndex, offsetBy: 8)...fullString.index(fullString.startIndex, offsetBy: 9)]) : ""
     }
     
     var hour: String {
-        String(fullString[fullString.index(fullString.startIndex, offsetBy: 11)...fullString.index(fullString.startIndex, offsetBy: 12)])
+        fullString.count > 14 ? String(fullString[fullString.index(fullString.startIndex, offsetBy: 11)...fullString.index(fullString.startIndex, offsetBy: 12)]) : ""
     }
     
     var minutes: String {
-        String(fullString[fullString.index(fullString.startIndex, offsetBy: 14)...fullString.index(fullString.startIndex, offsetBy: 15)])
+        fullString.count > 14 ? String(fullString[fullString.index(fullString.startIndex, offsetBy: 14)...fullString.index(fullString.startIndex, offsetBy: 15)]) : ""
     }
 }
